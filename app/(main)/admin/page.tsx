1a830473a257b833f871faabea957dbb794cda23
Bertin Selendo <contact@bertinselendo.com>
2024-03-16 20:06:01 +0100

    🎨 style: setup home page ui structure

--- app/(main)/admin/page.tsx
+++ app/(main)/admin/page.tsx
@@ -3,6 +3,9 @@ import { auth } from "@/lib/auth/helper";
 import { PageParams } from "@/types/next";
 import { redirect } from "next/navigation";
 import type { Metadata } from "next";
+import { AdminHomeCarousel } from "@/components/admin/home/projectCarousel";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Card, CardContent } from "@/components/ui/card";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -16,8 +19,20 @@ export default async function AdminHomePage(props: PageParams) {
 
   return (
     <div>
-      <UserAvatar src={user.avatar} />
-      <p>Bonjour {user.firstName}</p>
+      <div className="w-full px-24 border-b pb-4">
+        <AdminHomeCarousel />
+      </div>
+      <div className="py-6 px-20 flex flex-wrap gap-6 justify-center">
+        {Array.from({ length: 4 }).map((_, index) => (
+          <div key={index} className="w-[48%]">
+            <Card>
+              <CardContent className="flex items-center justify-center p-6 h-80">
+                <span className="text-2xl font-semibold">{index + 1}</span>
+              </CardContent>
+            </Card>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
