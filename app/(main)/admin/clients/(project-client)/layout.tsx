b5a3022a5dd652dfe5f2f6c4de0a9c1f76423c91
Bertin Selendo <contact@bertinselendo.com>
2024-03-19 21:24:35 +0100

    🎨 style: fix width

--- app/(main)/admin/clients/(project-client)/layout.tsx
+++ app/(main)/admin/clients/(project-client)/layout.tsx
@@ -12,14 +12,14 @@ export const metadata: Metadata = {
 export default async function RouteLayout(props: LayoutParams<{}>) {
   return (
     <div className="flex h-[90vh] overflow-scroll">
-      <div className="sticky top-0 overflow-scroll scroll custom-scrollbar xl:w-3/12 md:w-4/12 p-3 border-r">
+      <div className="sticky top-0 overflow-scroll scroll custom-scrollbar 2xl:w-3/12 md:w-4/12 p-3 border-r">
         <ListClients />
         <Button className="w-full mt-4">
           <Link href="/admin/clients/create">Create new client</Link>
         </Button>
       </div>
-      <div className="p-3 xl:w-9/12 md:w-7/12 flex justify-center">
-        <div className="w-1/2">{props.children}</div>
+      <div className="p-3 2xl:w-9/12 md:w-8/12 flex justify-center">
+        <div className="md:w-2/3 2xl:1/2">{props.children}</div>
       </div>
     </div>
   );
