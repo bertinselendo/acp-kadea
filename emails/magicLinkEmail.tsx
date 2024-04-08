f3285092357483468684721438fc29d7504cc5a1
Bertin Selendo <contact@bertinselendo.com>
2024-04-08 06:31:00 +0100

    🚀 feat: design email

--- emails/magicLinkEmail.tsx
+++ emails/magicLinkEmail.tsx
@@ -1,4 +1,11 @@
-import { Link, Preview, Section, Text } from "@react-email/components";
+import {
+  Button,
+  Heading,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
 import { EmailLayout } from "./utils/EmailLayout";
 
 export default function MagicLinkMail({ url }: { url: string }) {
@@ -7,20 +14,27 @@ export default function MagicLinkMail({ url }: { url: string }) {
       <Preview>
         You have requested a magic link to sign in to your account.
       </Preview>
-      <Section className="my-6">
-        <Text className="text-lg leading-6">
-          <Link className="text-sky-500 hover:underline" href={url}>
-            👉 Click here to sign in 👈
-          </Link>
+      <Section>
+        <Heading className="text-3xl font-bold my-7">
+          Access your account in one click
+        </Heading>
+        <Text className="text-lg mb-7">
+          Simply click on the link below to access your account - This link is
+          unique and can only be used once.
         </Text>
-        <Text className="text-lg leading-6">
-          If you didn request this, please ignore this email.
+
+        <Button
+          href={url}
+          className="w-[90%] p-4 rounded-lg text-center font-semibold bg-[#2eb67d] text-white"
+        >
+          Click here to sign in
+        </Button>
+
+        <Text>
+          If you didn&apos;t request this email, there&apos;s nothing to worry
+          about, you can safely ignore it.
         </Text>
       </Section>
-      <Text className="text-lg leading-6">
-        Best,
-        {/* <br />- {SiteConfig.maker.name} from {SiteConfig.title} */}
-      </Text>
     </EmailLayout>
   );
 }
--- emails/utils/EmailLayout.tsx
+++ emails/utils/EmailLayout.tsx
@@ -2,12 +2,17 @@ import { getServerUrl } from "@/lib/server-url";
 import {
   Body,
   Container,
+  Column,
   Head,
-  Hr,
+  Heading,
   Html,
   Img,
   Link,
+  Preview,
+  Row,
+  Section,
   Text,
+  Hr,
 } from "@react-email/components";
 import { Tailwind } from "@react-email/tailwind";
 import type { PropsWithChildren } from "react";
@@ -24,39 +29,131 @@ export const EmailLayout = ({ children }: PropsWithChildren) => {
     >
       <Html>
         <Head />
-        <Body className="bg-white font-sans">
-          <Container className="mx-auto bg-contain bg-bottom bg-no-repeat p-6">
-            <table cellPadding={0} cellSpacing={0}>
-              <tr>
-                <td className="pr-2">
-                  <Img
-                    src={`${baseUrl}`}
-                    width={32}
-                    height={32}
-                    className="inline"
-                    alt={`localhost's logo`}
-                  />
-                </td>
-                <td>
-                  <Text className="text-xl font-bold">Localhost</Text>
-                </td>
-              </tr>
-            </table>
+        {/* <Preview>Confirm your email address</Preview> */}
+        <Body className="bg-white mx-auto">
+          <Container className="mx-auto px-5">
+            <Section className="mt-9">
+              <Img
+                src={`${baseUrl}/logo.png`}
+                width="120"
+                height="36"
+                alt="Localhost"
+              />
+            </Section>
+
+            <Hr className="border-gray-300" />
+
             {children}
-            <Hr className="mt-12 border-gray-300" />
-            <Img
-              src={`${baseUrl}`}
-              width={32}
-              height={32}
-              className="inline"
-              alt={`localhost's logo`}
-            />
-            <Link href={`${baseUrl}/api/emails/unsubscribe`}>Unsubscribe</Link>
-            <Text className="ml-1 text-sm text-gray-500">Localhost</Text>
-            <Text className="ml-1 text-sm text-gray-500">france</Text>
+
+            <Section>
+              <Row className="mb-8 px-2 w-full">
+                <Column style={{ width: "66%" }}>
+                  <Img
+                    src={`${baseUrl}/static/slack-logo.png`}
+                    width="120"
+                    height="36"
+                    alt="Localhost"
+                  />
+                </Column>
+                <Column>
+                  <Section>
+                    <Row>
+                      <Column>
+                        <Link href="/">
+                          <Img
+                            src={`${baseUrl}/logo/twitter.png`}
+                            width="32"
+                            height="32"
+                            alt="Twitter"
+                            style={socialMediaIcon}
+                          />
+                        </Link>
+                      </Column>
+                      <Column>
+                        <Link href="/">
+                          <Img
+                            src={`${baseUrl}/logo/facebook.png`}
+                            width="32"
+                            height="32"
+                            alt="Facebook"
+                            style={socialMediaIcon}
+                          />
+                        </Link>
+                      </Column>
+                      <Column>
+                        <Link href="/">
+                          <Img
+                            src={`${baseUrl}/logo/linkedin.png`}
+                            width="32"
+                            height="32"
+                            alt="linkedin"
+                            style={socialMediaIcon}
+                          />
+                        </Link>
+                      </Column>
+                    </Row>
+                  </Section>
+                </Column>
+              </Row>
+            </Section>
+
+            <Section>
+              <Link
+                style={footerLink}
+                href="http://localhost:3000"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Website
+              </Link>
+              &nbsp;&nbsp;&nbsp;·&nbsp;&nbsp;&nbsp;
+              <Link
+                style={footerLink}
+                href="http://localhost:3000/privacy"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Policies
+              </Link>
+              &nbsp;&nbsp;&nbsp;·&nbsp;&nbsp;&nbsp;
+              <Link
+                style={footerLink}
+                href="http://localhost:3000/help"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Help center
+              </Link>
+              &nbsp;&nbsp;&nbsp;·&nbsp;&nbsp;&nbsp;
+              <Link
+                style={footerLink}
+                href="http://localhost:3000/term"
+                target="_blank"
+                rel="noopener noreferrer"
+                data-auth="NotApplicable"
+                data-linkindex="6"
+              >
+                Term
+              </Link>
+              <Text className="text-xs text-[#b7b7b7] mb-12">
+                ©2024 Localhost Technologies, LLC, a Salesforce company. <br />
+                500 Howard Street, San Francisco, CA 94105, USA <br />
+                <br />
+                All rights reserved.
+              </Text>
+            </Section>
           </Container>
         </Body>
       </Html>
     </Tailwind>
   );
+};
+
+const footerLink = {
+  color: "#b7b7b7",
+};
+
+const socialMediaIcon = {
+  display: "inline",
+  marginLeft: "32px",
 };
