e28072568eea7abfb5cbb1d3ee1f782fefcd47f8
Bertin Selendo <contact@bertinselendo.com>
2024-04-21 19:32:24 +0100

    🚀 feat: link client & user in Organization on creation

--- src/components/admin/clients/clients.action.ts
+++ src/components/admin/clients/clients.action.ts
@@ -9,9 +9,9 @@ import { User } from "@prisma/client";
 export async function createClientUser(values: User, clientID: string) {
   const user = await auth();
 
-  if (!user) {
-    return;
-  }
+  if (!user) return;
+
+  if (!user.organizationId) return;
 
   try {
     const findUser = await prisma.user.findUnique({
@@ -40,19 +40,20 @@ export async function createClientUser(values: User, clientID: string) {
       }
     }
 
-    const user = await prisma.user.create({
+    const newUser = await prisma.user.create({
       data: {
         firstName: values.firstName,
         lastName: values.lastName,
         email: values.email,
         role: "CLIENT",
         clientId: clientID,
+        organizationId: user.organizationId,
       },
       include: {
         client: true,
       },
     });
-    return user;
+    return newUser;
   } catch (error) {
     if (error.meta?.target[0] == "email") {
       throw new Error("User already exist");
