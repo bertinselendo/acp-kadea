fd960ce613be924990231e92cb15d2857281b539
Bertin Selendo <contact@bertinselendo.com>
2024-05-01 00:59:23 +0100

    🚀 feat: add read projects for users

--- express/controllers/user/u-read.ts
+++ express/controllers/user/u-read.ts
@@ -45,3 +45,63 @@ export const getUserClient = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Error getting client" });
   }
 };
+
+export const getUserProjects = async (req: Request, res: Response) => {
+  const userID = req.params.id;
+  const user = res.locals.session?.user;
+
+  if (!userID) {
+    return BadRequestError(res, "Bad Request: missing user id");
+  }
+
+  try {
+    if (user.role == "ADMIN" || user.role == "MANAGER") {
+      const projects = await prisma.project.findMany({
+        include: {
+          client: true,
+        },
+      });
+      res.status(201).json(projects);
+    } else if (user.role == "WORKER") {
+      const projects = await prisma.teamMember
+        .findFirst({
+          where: {
+            user: {
+              id: userID,
+            },
+          },
+          include: {
+            projects: {
+              include: {
+                client: true,
+              },
+            },
+          },
+        })
+        .then((result) => result?.projects);
+      res.status(201).json(projects);
+    } else if (user.role == "CLIENT") {
+      const projects = await prisma.client
+        .findFirst({
+          where: {
+            users: {
+              some: {
+                id: userID,
+              },
+            },
+          },
+          include: {
+            projects: {
+              include: {
+                client: true,
+              },
+            },
+          },
+        })
+        .then((result) => result?.projects);
+      res.status(201).json(projects);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error getting user projects" });
+  }
+};
--- express/routes/user.ts
+++ express/routes/user.ts
@@ -1,5 +1,9 @@
 import express from "express";
-import { getUserByEmail, getUserClient } from "../controllers/user/u-read";
+import {
+  getUserByEmail,
+  getUserClient,
+  getUserProjects,
+} from "../controllers/user/u-read";
 import { deleteUser } from "../controllers/user/u-delete";
 
 const userRoute = express.Router();
@@ -7,6 +11,7 @@ const userRoute = express.Router();
 // GET
 userRoute.get("/email/:email", getUserByEmail);
 userRoute.get("/:id/client", getUserClient);
+userRoute.get("/:id/projects", getUserProjects);
 
 // DELETE
 userRoute.delete("/:id", deleteUser);
