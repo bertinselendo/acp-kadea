3e53bbbfb2445605903021ba286798e4ea970144
bertinselendo <contact@bertinselendo.com>
2024-05-12 02:52:51 +0100

    🛠️ fix: title and blog construction page

--- nextjs/app/(pages)/blog/page.tsx
+++ nextjs/app/(pages)/blog/page.tsx
@@ -1,7 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { bgGradient } from "@/lib/utils";
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Onboarding",
+};
 
 export default async function BlogPage() {
   return (
@@ -12,43 +17,26 @@ export default async function BlogPage() {
           className={`w-full justify-center transition-all
         ${bgGradient().noise}`}
         >
-          <div className="mx-auto flex w-10/12 flex-col items-center justify-center gap-4 pt-20 md:w-8/12 md:pt-32">
-            <Button className="bg-primary/30 text-xs">
-              by agency for agencies
-            </Button>
-            <div className="space-y-4 text-center">
-              <h1 className="text-4xl font-black md:text-7xl">
-                Manage your customers resources simply and centrally
-              </h1>
-              <p>
-                From feedback to documents, including invoices and access to
-                project accounts.
-              </p>
-              <Button asChild>
-                <Link href="/onboarding" className="text-base font-semibold">
-                  Try for free
-                </Link>
-              </Button>
-              <div className="mx-auto flex items-center justify-center gap-1">
-                <Image
-                  src="/pages/stars.png"
-                  alt=""
-                  width="136"
-                  height="28"
-                  className="h-6 w-auto"
-                />
-                <span>4.9 out of 5</span>
-              </div>
-              <div>
-                <Image
-                  src="/pages/client-portal-mockup.png"
-                  alt=""
-                  width="1920"
-                  height="1080"
-                />
-              </div>
+          <div className="mx-auto flex w-10/12 flex-col items-center justify-center gap-4 pt-10 md:w-8/12">
+            <div className="my-10 space-y-4 text-center md:my-16">
+              <h1 className="text-4xl font-black md:text-7xl">Blog</h1>
             </div>
           </div>
+        </div>
+      </div>
+
+      {/* On dev */}
+      <div
+        id="features"
+        className="features space-y-6 py-10 text-center md:py-20"
+      >
+        <div className="mx-auto w-10/12 text-center md:w-7/12">
+          <h2 className="text-2xl font-extrabold md:text-4xl">
+            Page under construction
+          </h2>
+          <p className="mx-10 md:mx-32">
+            We&apos;re working to bring you an optimal experience soon
+          </p>
         </div>
       </div>
     </>
